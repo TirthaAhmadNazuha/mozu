f38fbc3a97d28473de35c8d629f288d6d977c575
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2022-02-17 01:11:54 +0700

    merubah desaign lagi

--- app.js
+++ app.js
@@ -1,63 +1,50 @@
 $(document).ready(function () {
-  function sapaUser(data) {
-    $("#welcome").html(`Hello!!, ${data}`);
-  }
-  $("#welcome").html(`Hello!!, ${localStorage.getItem("namamu")}`);
-  $(".bottomNav .item").click(function () {
-    $(this).addClass("on").siblings().removeClass("on");
-  });
-  $(".jumbotron .left p").click(function () {
-    $(this).toggleClass("on");
-  });
-  setTimeout(() => {
-    $(`.firstPage .page.d1`).addClass("on");
-  }, 300);
-  let fpi = 1,
-    namval = false;
-  if (localStorage.getItem("daftar") == "true") {
-    $(".firstPage").css("display", "none");
-  }
-  $(".firstPage .next").click(function () {
-    fpi++;
-    $(`.firstPage .indicator .item:nth-child(${fpi})`)
-      .addClass("on")
-      .siblings()
-      .removeClass("on");
-    $(`.firstPage .page.d${fpi}`).addClass("ro").siblings().removeClass("ro");
-    setTimeout(() => {
-      if (fpi != 4) $(`.firstPage .page.d${fpi - 1}`).addClass("off");
-      setTimeout(() => {
-        $(`.firstPage .page.d${fpi}`)
-          .addClass("on")
-          .siblings()
-          .removeClass("on");
-      }, 10);
-    }, 10);
-    if (fpi == 3) {
-      setTimeout(() => {
-        $(".firstPage .page.d3 .inputname input").focus();
-      }, 400);
-    }
-    if (fpi == 4 && namval) {
-      fpi = 4;
-      lagi = true;
-      $(".firstPage").fadeOut(400);
-      localStorage.setItem(
-        "namamu",
-        $(".firstPage .page.d3 .inputname input").val()
-      );
-      localStorage.setItem("daftar", true);
-      sapaUser(localStorage.getItem("namamu"));
-      console.log(nameMe);
-    } else if (fpi == 4 && !namval) {
-      fpi = 3;
-      $(".firstPage .page.d3 .inputname label").html(
-        "please enter your name!!.."
-      );
-    }
-    console.log(fpi);
-  });
-  $(".firstPage .page.d3 .inputname input").keyup(function () {
-    if ($(this).val() != "") namval = true;
-  });
+    $('.nav .search form').submit((e) => {
+        e.preventDefault()
+    })
+    $('.nav .inputSearch .reset').click(function () {
+        $('.nav .inputSearch input').val('').focus()
+        $(this).css('display', 'none')
+    })
+    $('.nav .inputSearch input').keyup(() => {
+        if ($('.nav .inputSearch input').val() != '') {
+            $('.nav .inputSearch .reset').css('display', 'block')
+        } else {$('.nav .inputSearch .reset').css('display', 'none')}
+    })
+    $('.nav .menu').click(function () {
+        $('.ulMenu').toggleClass('d-none');
+        setTimeout(() => {
+            $('.ulMenu').toggleClass('off')
+        }, 10);
+    })
+    $('.tema').click(function () {
+        $('.tema i').toggleClass('d-none')
+        if ($('.tema .fi-rr-moon').hasClass('d-none')) {
+            $(':root').css('--bg', 'var(--dark)')
+            $(':root').css('--color', 'var(--colorDark)')
+            $(':root').css('--body', 'var(--bgDark)')
+            $('.nav .search').css('background', 'var(--body)')
+        } else {
+            $(':root').css('--bg', 'var(--light)')
+            $(':root').css('--color', 'var(--colorLight)')
+            $(':root').css('--body', 'var(--bgLight)')
+            $('.nav .search').css('background', '#0001')
+        }
+    });
+    $('.nav .menu').click(function () {
+        $('.sekunderNav.side').toggleClass('off');
+    });
+
+    if ($(window).width() >= 768) {
+        $('.sekunderNav').removeClass('bottom').addClass('side');
+    } else $('.sekunderNav').removeClass('side').addClass('bottom') 
+    $(window).on("resize", () => {
+        if ($(this).width() >= 768) {
+            $('.sekunderNav').removeClass('bottom').addClass('side');
+        } else $('.sekunderNav').removeClass('side').addClass('bottom') 
+    }).on("load", () => {
+        if ($(this).width() >= 768) {
+            $('.sekunderNav').removeClass('bottom').addClass('side');
+        } else $('.sekunderNav').removeClass('side').addClass('bottom') 
+    });
 });
