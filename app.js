4cf3f2ba0da25326aeec885f5f15ef2e7b24279b
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2022-02-18 01:18:59 +0700

    menghapus program dragging menu mobile karena terlalu berat

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
 $(document).ready(function () {
-    let ulMenuPos = {
-        y: 0,
-        eleBottom: 0
-    }
-    let dy = 0
-    let ulMenuHeight = -parseInt($('.con .ulMenu').css('height'), 10);
-
-    $('.con .ulMenu').on('touchstart', (e) => {
-        $('.con .ulMenu').css('transition', 'none');
-        if (parseInt($('.con .ulMenu').css('bottom'), 10) > 0) $('.con .ulMenu').css('bottom', '0')
-        ulMenuPos = {
-            y: e.targetTouches[0].clientY,
-            eleBottom: parseInt($('.con .ulMenu').css('bottom'), 10)
-        }
-        $(this).bind('touchmove', (e) => {
-            dy = e.targetTouches[0].clientY - ulMenuPos.y;
-            $('.con .ulMenu').css('bottom', ulMenuPos.eleBottom + -dy + 'px');
-            if (parseInt($('.con .ulMenu').css('bottom'), 10) > 0) $('.con .ulMenu').css('bottom', '0')
-
-        })
-        // console.log(e.targetTouches[0].clientY)
-    }).on('touchend', (e) => {
-        $('.con .ulMenu').css('transition', '310ms ease');
-        if (parseInt($('.con .ulMenu').css('bottom'), 10) > 0) $('.con .ulMenu').css('bottom', '0')
-        if (parseInt($('.con .ulMenu').css('bottom'), 10) < ulMenuHeight / 2) {
-            $('.con .ulMenu').css('bottom', `calc(${ulMenuHeight + 'px'} + -10vh)`)
-        } else {
-            $('.con .ulMenu').css('bottom', '-1px')
-        }
-            
-        ulMenuPos.eleBottom = parseInt($('.con .ulMenu').css('bottom'), 10)
-        console.log('Sayang pasti bisa !!', parseInt($('.con .ulMenu').css('bottom'), 10))
-    })
+    
 
     $('.nav .search form').submit((e) => {
         e.preventDefault()
@@ -63,6 +31,7 @@ $(document).ready(function () {
     $('.nav .menu').click(function () {
         $('.sekunderNav.side').toggleClass('off');
         $('.con .ulMenu').toggleClass('off')
+        $('.con .ulMenu .conItems').scrollTop(0);
     });
 
     if ($(window).width() >= 768) {
