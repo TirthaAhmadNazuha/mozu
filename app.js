2b12854674d3c9020be77e4591a4e38b0a10bd22
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2022-02-25 01:23:00 +0700

    complete search box algorthm

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    //bg perspectif function
+    $('.contenCon .home :is(.bg, h1, p, .jumbotron, .bg div)').on('mousemove', (e) => {
+        const pos = {
+            x: e.clientX,
+            y: e.clientY
+        }
+        const position = $('.contenCon .home :is(.bg, h1, p, .jumbotron)').position();
+        const top = position.top;
+        const left = position.left;
+        
+        const center = {
+            x: left + $('.contenCon .home :is(.bg, h1, p, .jumbotron)').width() / 2,
+            y: top + $('.contenCon .home :is(.bg, h1, p, .jumbotron)').height() / 2 
+        }
+        let hasil = {
+            x: (pos.x - center.x) / 60,
+            y: (-pos.y - -center.y) / 30
+        }
+        // append hasil
+        $('.contenCon .home .bg').css('transform', `rotateX(${hasil.y}deg) rotateY(${hasil.x}deg)`)
+    })
+
+
 
     let formDataBases = []
     const coba = [
@@ -10,8 +33,9 @@ $(document).ready(function () {
     $.getJSON("data/items.json", (data) => {
         formDataBases = data
         formDataBases.forEach(item => {
+            item.id = Math.floor(100000 + Math.random() * 900000)
             $('.product .productCon').append(`
-                    <div class="items" data-title="${item.nama}">${item.nama}</div>
+                    <div class="items" id="${item.id}" data-title="${item.keyword}">${item.nama}</div>
                 `)
         });
     });
@@ -21,28 +45,37 @@ $(document).ready(function () {
         $('.contenCon .product').removeClass('d-none').siblings().addClass('d-none')
         $('head title').html('Search | Mozu')
         $('.sekunderNav .search').addClass('on').siblings().removeClass('on')
-        const inputSearching = formDataBases.filter((item) => {
-            return item.nama.toLowerCase().indexOf(value) >= 0
-        })
-        let items = document.querySelectorAll('.product .productCon .items')
-        items.forEach(hide => hide.style.display = 'none')
-        items.forEach(filter => {
-            let title = filter.getAttribute('data-title')
-            if (inputSearching[0].nama == title && value.length >= 1) {
-                filter.style.display = 'block'
-            }
-            if (value.length <= 1) {
-                filter.style.display = 'block'
-            }
+        // const inputSearching = formDataBases.filter((item) => {
+        //     return item.nama.toLowerCase().indexOf(value) >= 0
+        // })
+        // let items = document.querySelectorAll('.product .productCon .items')
+        // items.forEach(hide => hide.style.display = 'none')
+        // items.forEach(filter => {
+        //     let title = filter.getAttribute('data-title')
+        //     if (inputSearching[0].nama == title && value.length >= 1) {
+        //         filter.style.display = 'block'
+        //     }
+        //     if (value.length <= 1) {
+        //         filter.style.display = 'block'
+        //     }
+        // })
+        $('.contenCon .product .items').css('display', (value) ? 'none' : 'flex')
+        formDataBases.forEach(item => {
+            $(`.contenCon .product .items#${item.id}`).filter(() => {
+                let i = false
+                if ($(`.contenCon .product .items#${item.id}`).attr('data-title').toLowerCase().indexOf(value) > -1) i = true
+                console.log($(`.contenCon .product .items#${item.id}`).attr('data-title'), $(`.contenCon .product .items#${item.id}`).text())
+                if (i) $(`.contenCon .product .items#${item.id}`).css('display', 'flex')
+            })
         })
     })
-    $('.nav .inputSearch .reset, .product .inputSearch .reset').click(function () {
+    $('.nav .inputSearch .reset').click(function () {
         $('.nav .inputSearch input, .product .inputSearch input').val('').focus()
         $(this).css('display', 'none')
     })
     $('.nav .inputSearch input').keyup(() => {
-        if ($('.nav .inputSearch input').val() != '') {
-            $('.nav .inputSearch .reset').css('display', 'block')
+        if ($('.nav .inputSearch input').val()) {
+            $('.nav .inputSearch .reset').css('display', 'flex')
         } else {$('.nav .inputSearch .reset').css('display', 'none')}
     });
     $('.tema').click(function () {
@@ -109,7 +142,7 @@ $(document).ready(function () {
         }
         if ($('.sekunderNav.side .troli').hasClass('on')) {
             $('.contenCon .cart').removeClass('d-none').siblings().addClass('d-none')
-            $('head title').html('Troli | Mozu')
+            $('head title').html('Cart | Mozu')
             $(window).scrollTop(0)
         }
         if ($('.sekunderNav.side .contact').hasClass('on')) {
@@ -124,7 +157,7 @@ $(document).ready(function () {
         }
         if ($('.sekunderNav.side .tandai').hasClass('on')) {
             $('.contenCon .simpan').removeClass('d-none').siblings().addClass('d-none')
-            $('head title').html('Di tandai | Mozu')
+            $('head title').html('Bookmark | Mozu')
             $(window).scrollTop(0)
         }
         if ($('.sekunderNav.side .blogs').hasClass('on')) {
@@ -152,7 +185,7 @@ $(document).ready(function () {
         }
         if ($('.sekunderNav.bottom .troli').hasClass('on')) {
             $('.contenCon .cart').removeClass('d-none').siblings().addClass('d-none')
-            $('head title').html('Troli | Mozu')
+            $('head title').html('Cart | Mozu')
             $('.nav').removeClass('onSearch')
             $(window).scrollTop(0)
         }
@@ -170,7 +203,7 @@ $(document).ready(function () {
         }
         if ($('.sekunderNav.bottom .tandai').hasClass('on')) {
             $('.contenCon .simpan').removeClass('d-none').siblings().addClass('d-none')
-            $('head title').html('Di tandai | Mozu')
+            $('head title').html('Bookmark | Mozu')
             $('.nav').removeClass('onSearch')
             $(window).scrollTop(0)
         }
